25b6460e61d74ba246d40493a1f928d8696ba07b
Marzena <marzena_szopinska@hotmail.com>
2017-12-13 23:58:32 +0000

    changed SearchBar into class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-
-const SearchBar = () => {
-    return (
-        <input />
-    );
+class SearchBar extends Component {
+    render() {
+        return <input />
+    }
 }
 // make it accessable to other files that import SearchBar component
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -12,4 +12,5 @@ const App = () => {
     );
 }
 
+
 ReactDOM.render(<App />, document.querySelector('.container'));
