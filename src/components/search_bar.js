a99b05bf4c9233dc877b9150702251ecee23b8ea
Marzena <marzena_szopinska@hotmail.com>
2017-12-14 07:45:28 +0000

    added state inside of SearchBar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        // set default value for this state
+        this.state = { term: '' };
+
+    }
+    // instead of e => console.log(e.target.value) you can also use
+    // this method and pass it inside of onChange
+    //onInputChange(e) {
+    //    console.log(e.target.value);
+    //}
+
     render() {
-        return <input />
+        
+        return (
+            <div>
+                {/* update state and rerender whenever event onChange occures */}
+                <input value={this.state.term} onChange={e => this.setState({ term: e.target.value})}/>
+                <p>The input value: {this.state.term}</p>
+            </div>
+        );
     }
 }
 // make it accessable to other files that import SearchBar component
